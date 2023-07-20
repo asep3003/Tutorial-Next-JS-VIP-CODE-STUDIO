71cf49b372a9ba7b6968aa9e628979b072cc0f00
asep3003 <saepulloha133@gmail.com>
2023-07-20 23:52:50 +0700

    Pages & Layout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,3 @@
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
-
 export default function Home() {
   return (
     <div>
@@ -9,5 +5,5 @@ export default function Home() {
         Hello Asep
       </div>
     </div>
-  )
+  );
 }
